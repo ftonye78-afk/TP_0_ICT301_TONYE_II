918c3ee71dd785daec2c417b1fcfd3a85bb1dcf5
ftonye78-afk <ftonye78@gmail.com>
2025-12-25 15:46:09 +0100

    Update OrderProcessor.java

--- DIP/OrderProcessor.java
+++ DIP/OrderProcessor.java
@@ -1,3 +1,15 @@
+
+package ict301.solid.dip;
+
+class MySQLDatabase {
+    public void insert(String data) {}
+}
+
 public class OrderProcessor {
+    
     private MySQLDatabase database = new MySQLDatabase();
+
+    public void process(String order) {
+        database.insert(order);
+    }
 }

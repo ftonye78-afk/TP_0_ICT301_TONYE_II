30d04278f092a8e81cdac8899d6de67590d68e4a
ftonye78-afk <ftonye78@gmail.com>
2025-12-25 15:48:29 +0100

    Update DipRefactored.java

--- DIP/DipRefactored.java
+++ DIP/DipRefactored.java
@@ -1,4 +1,26 @@
-public class OrderProcessor {
-    private Database database;
-    public OrderProcessor(Database database) { this.database = database; }
+package ict301.solid.dip;
+
+interface Database {
+    void insert(String data);
 }
+
+class MySQLDatabase implements Database {
+    public void insert(String data) { System.out.println("Save in MySQL"); }
+}
+
+class MongoDBDatabase implements Database {
+    public void insert(String data) { System.out.println("Save in MongoDB"); }
+}
+
+public class OrderProcessorRefactored {
+    private Database database;
+
+    public OrderProcessorRefactored(Database db) {
+        this.database = db;
+    }
+
+    public void process(String order) {
+        database.insert(order);
+    }
+}
+

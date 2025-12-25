64230fdd50311703502f76da037011e0f7387fd4
ftonye78-afk <ftonye78@gmail.com>
2025-12-25 15:51:20 +0100

    Update IspRefactored.java

--- ISP/IspRefactored.java
+++ ISP/IspRefactored.java
@@ -1,2 +1,18 @@
-interface Workable { void work(); }
-interface Eatable extends Workable { void eat(); }
+package ict301.solid.isp;
+
+interface Workable {
+    void work();
+}
+
+interface Eatable {
+    void eat();
+}
+
+class HumanWorker implements Workable, Eatable {
+    public void work() { System.out.println("L'humain travaille."); }
+    public void eat() { System.out.println("L'humain mange."); }
+}
+
+class RobotWorker implements Workable {
+    public void work() { System.out.println("Le robot travaille."); }
+}

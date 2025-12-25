9d62ab3284c0417308383357e3928ca6a115bef5
ftonye78-afk <ftonye78@gmail.com>
2025-12-25 15:49:59 +0100

    Update Worker.java

--- ISP/Worker.java
+++ ISP/Worker.java
@@ -1,1 +1,20 @@
-public interface Worker { void work(); void eat(); }
+package ict301.solid.isp;
+
+public interface Worker {
+    void work();
+    void eat();
+}
+
+class HumanWorker implements Worker {
+    public void work() { System.out.println("L'humain travaille."); }
+    public void eat() { System.out.println("L'humain mange."); }
+}
+
+class RobotWorker implements Worker {
+    public void work() { System.out.println("Le robot travaille."); }
+    public void eat() { 
+        
+        throw new UnsupportedOperationException(); 
+    }
+}
+

5bce9b1b15372f0f76bc2e730fe1964d0fd1d784
ftonye78-afk <ftonye78@gmail.com>
2025-12-25 16:01:45 +0100

    Create Book.java

---
+++ SRP/Book.java
@@ -0,0 +1,9 @@
+package ict301.solid.srp;
+
+public class Book {
+    private String title, author, text;
+
+    public String getTitle() { return title; }
+    public void printToScreen() { System.out.println(text); }
+    public void saveToFile() { /* code pour sauvegarder */ }
+}
